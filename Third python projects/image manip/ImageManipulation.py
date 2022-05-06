524b1985de1a561f99160039ce099a6d2420a5bd
RajAChow <102255617+RajAChow@users.noreply.github.com>
2022-05-05 21:29:05 -0700

    Update ImageManipulation.py

--- Third python projects/image manip/ImageManipulation.py
+++ Third python projects/image manip/ImageManipulation.py
@@ -1,8 +1,8 @@
-from PIL import Image
-import os
+from PIL import Image, ImageFilter, ImageEnhance
+import os, sys
 
 j_List = ["arch", "balloons", "dubai", "ems", "helix", "kcm", "landscape", "palace", "rio", "trees"]
-a_List = ["rotate", "resize", "png", "black+white", "blur"]
+a_List = ["rotate", "resize", "png", "black+white", "blur", "contrast"]
 size200 = (200,200)
 size400 = (400,400)
 size600 = (600,600)
@@ -15,29 +15,32 @@ def displayList(x):
 def main():
     while True:
         global userImageChoice
-        userImageChoice = ""
+        userImageChoice = None
         print("Saved images: ")
         displayList(j_List)
         userImageChoice = input("Which image would u like to open? or (q) to quit: ")
-        userImageChoice.lower()       
-        if userImageChoice in j_List:
+        userImageChoice.lower()  
+        if userImageChoice == "q":
+            break     
+        elif userImageChoice in j_List:
             userImage = Image.open(f"{userImageChoice}.jpg")
             userImage.show()
-            if userConfirm() == True:
+            x = userConfirm()
+            if x == False:
+                continue
+            elif x == True:
                 alterUserImage()
-        elif userImageChoice == "q":
-            break
         else:
             print("Invalid input\n")
     
 def userConfirm():
     while True:
-        userChoice = input("do you want to alter this image? (y/n): ")
+        userChoice = input("do you want to alter the image? (y/n): ")
         if userChoice.lower() == "y":
             break
         elif userChoice.lower() == "n":
-            print("")
-            main()
+            userImageChoice = None
+            return False
         else:
             print("Invalid input\n")
     return True
@@ -65,57 +68,74 @@ def alterUserImage():
                 elif userChoice == a_List[4]:
                     blur()   
                     break
+                elif userChoice == a_List[5]:
+                    contrast() 
+                    break
         else:
             print("Invalid input\n")
 
 def rotate():
+    while True:
+        try:
+            userDegrees = int(input("How many degrees would you like to rotate?: "))
+            break
+        except ValueError:
+            print("Invalid input\n")
     im = Image.open(f"{userImageChoice}.jpg")
-    im.rotate(90).save('rotation folder/'+userImageChoice+'Rotated.jpg')
-    p_List.append(userImageChoice)
-    print("Saved in rotation folder")
+    im.rotate(userDegrees).save('rotation folder/'+userImageChoice+'Rotated.jpg')
+    print("Saved in rotation folder\n")
 
 def resize():
     while True:
-    userChoice = input("Do you want it to be 200x200, 400x400, 600x600?: ")
-    im = Image.open(f"{userImageChoice}.jpg")
-    userChoice.lower()
-    if userChoice == "200x200":
-        im.thumbnail(size200)
-        im.save('200 jpeg/'+userImageChoice+'200.jpg')
-        p_List.append(userImageChoice)
-        print("Saved in 200 jpeg folder")
-        break
-    elif userChoice == "400x400":
-        im.thumbnail(size400)
-        im.save('400 jpeg/'+userImageChoice+'400.jpg')
-        p_List.append(userImageChoice)
-        print("Saved in 400 jpeg folder")
-        break
-    elif userChoice == "600x600":
-        im.thumbnail(size600)
-        im.save('600 jpeg/'+userImageChoice+'600.jpg')
-        p_List.append(userImageChoice)
-        print("Saved in 400 jpeg folder")
-        break
-    else:
-        print("Invalid input")
+        userChoice = int(input("Do you want it to be 200, 400, 600: "))
+        im = Image.open(f"{userImageChoice}.jpg")
+        if userChoice == 200:
+            im.thumbnail(size200)
+            im.save('200 jpeg/'+userImageChoice+'200.jpg')
+            print("Saved in 200 jpeg folder\n")
+            break
+        elif userChoice == 400:
+            im.thumbnail(size400)
+            im.save('400 jpeg/'+userImageChoice+'400.jpg')
+            print("Saved in 400 jpeg folder\n")
+            break
+        elif userChoice == 600:
+            im.thumbnail(size600)
+            im.save('600 jpeg/'+userImageChoice+'600.jpg')
+            print("Saved in 600 jpeg folder\n")
+            break
+        else:
+            print("Invalid input")
 
 def png():
     im = Image.open(f"{userImageChoice}.jpg")
     fn, fext = os.path.splitext(f"{userImageChoice}.jpg")
     im.save('png folder/{}.png'.format(fn))
-    print("Saved in png folder")
-    p_List.append(userImageChoice)
+    print("Saved in png folder\n")
 
 def blackWhite():
     im = Image.open(f"{userImageChoice}.jpg")
-    im = img.convert("L")
+    im = im.convert("L")
     im.save('bw folder/'+userImageChoice+'BW.jpg')
-    p_List.append(userImageChoice)
-    print("Saved in bw folder")
-    return
-
+    print("Saved in bw folder\n")
+    
 def blur():
-    return
+    while True:
+        try:
+            userBlur = float(input("How much would you like to blur (Choose a number)?: "))
+            break
+        except ValueError:
+            print("Invalid input\n")
+    im = Image.open(fr"{userImageChoice}.jpg")    
+    im1 = im.filter(ImageFilter.GaussianBlur(userBlur))
+    im1.save('blur folder/'+userImageChoice+'Blur.jpg')
+    print("Saved in blur folder\n")
+
+def contrast():
+    im = Image.open(f"{userImageChoice}.jpg")
+    im.save('contrast folder/'+userImageChoice+'Contrast.jpg')
+    im1 = ImageEnhance.Contrast(im)
+    con_im = im1.enhance(1.3).show("30% more contrast")
+    con_im.save('contrast folder/'+userImageChoice+'Contrast.jpg')
 
 main()
