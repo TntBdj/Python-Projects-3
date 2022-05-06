b74e52c2e254dbeeb5341cab6bcae38a8cf07597
RajAChow <102255617+RajAChow@users.noreply.github.com>
2022-05-05 23:55:23 -0700

    Update PasswordManager.py

--- Third python projects/PasswordManager.py
+++ Third python projects/PasswordManager.py
@@ -1,33 +1,19 @@
+from unicodedata import name
 from cryptography.fernet import Fernet
-
-"""
-def write_key():
-        key = Fernet.generate_key()
-        with open("key.key", "wb") as key_file:
-                key_file.write(key) 
-"""
 
 master_pass = input("Enter the master password (be sure to remember it): ")
 
-def load_key():
+def new_key():
         file = open("key.key", "rb")
         key = file.read()
         file.close()
         return key
 
-key = load_key() 
+key = new_key() 
 fer = Fernet(key)
 
 
-def view():
-    with open('password_database.txt', 'r') as f:
-            for line in f.readlines():
-                data = line.rstrip()
-                user, passw, phoneN = data.split("|")
-                print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode(), "| Phone Number:", fer.decrypt(phoneN.encode()).decode())
-
-
-def add():
+def add_info():
     name = input("Account name: ")
     pwd = input("Password: ")
     phone = input("Phone number: ")
@@ -36,13 +22,20 @@ def add():
         f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "|" + fer.encrypt(phone.encode()).decode() + "\n")
 
 
+def view_info():
+    with open('password_database.txt', 'r') as f:
+            for line in f.readlines():
+                data = line.rstrip()
+                name, password, phoneNumber = data.split("|")
+                print("Name:", name, "| Password:", fer.decrypt(password.encode()).decode(), "| Phone Number:", fer.decrypt(phoneNumber.encode()).decode())
+
+
 while True:
-    mode = input("View or add a password (v/a) or q to quit?: ")
-    mode.lower()
-    if mode == "q":
+    userSelection = input("View or add a password (v/a) or q to quit?: ")
+    userSelection.lower()
+    if userSelection == "q":
             break
-        
-    if mode == "v":
+    elif userSelection == "v":
         while True:
                 userInput = input("Enter the master password before viewing information: ")
                 if userInput == master_pass:
@@ -52,8 +45,8 @@ while True:
                 else:
                         print("Sorry that password is incorrect, try again or press q to quit")
                         continue
-        view()
-    elif mode == "a":
+        view_info()
+    elif userSelection == "a":
         while True:
                 userInput = input("Enter the master password before adding information: ")
                 if userInput == master_pass:
@@ -63,7 +56,7 @@ while True:
                 else:
                         print("Sorry that password is incorrect, try again or press q to quit")
                         continue
-        add()
+        add_info()
     else:
         print("Invalid input")
         continue
