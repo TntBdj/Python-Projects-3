3031824453aa9709cb2ee66823bcb15e0ea8e4ea
RajAChow <102255617+RajAChow@users.noreply.github.com>
2022-05-05 23:38:42 -0700

    Update ImageManipulation.py

--- Third python projects/image manip/ImageManipulation.py
+++ Third python projects/image manip/ImageManipulation.py
@@ -1,38 +1,27 @@
+#Importing all these modules is required for the code to run completely
 from PIL import Image, ImageFilter, ImageEnhance
 import os, sys
 
+
+#The two lists are used so repetition of typing the words is avoided
 j_List = ["arch", "balloons", "dubai", "ems", "helix", "kcm", "landscape", "palace", "rio", "trees"]
 a_List = ["rotate", "resize", "png", "black+white", "blur", "merge", "contrast"]
-size200 = (200,200)
-size400 = (400,400)
-size600 = (600,600)
 
+
+""" 
+This is a simple for loop that displays the above lists 
+because I don't like how lists are normally displayed
+"""
 def displayList(x):
     for i in x:
         print (i)
     print("")
-        
-def main():
-    while True:
-        global userImageChoice
-        userImageChoice = None
-        print("Saved images: ")
-        displayList(j_List)
-        userImageChoice = input("Which image would u like to open? or (q) to quit: ")
-        userImageChoice.lower()  
-        if userImageChoice == "q":
-            break     
-        elif userImageChoice in j_List:
-            userImage = Image.open(f"{userImageChoice}.jpg")
-            userImage.show()
-            x = userConfirm()
-            if x == False:
-                continue
-            elif x == True:
-                alterUserImage()
-        else:
-            print("Invalid input\n")
-    
+
+
+""" 
+Assures that the user wants to use the selected/shown image, if they select no they 
+will be prompted to choose another image, if they select yes the program will continue
+"""
 def userConfirm():
     while True:
         userChoice = input("do you want to alter the image? (y/n): ")
@@ -46,12 +35,46 @@ def userConfirm():
             print("Invalid input\n")
     return True
 
+
+#This is the primary function that eventually leads to every other function being called        
+def main():
+    while True:
+        #This makes the userImageChoice var global while still being intially declared in this function
+        global userImageChoice
+        #Assures the var never is an unintended value
+        userImageChoice = None
+        print("Saved images: ")
+        #This displays the contents of the j(peg) list how I previously mentioned
+        displayList(j_List)
+        #Asks for the image the user wants to use for the rest of the program
+        userImageChoice = input("Which image would u like to open? or (q) to quit: ")
+        userImageChoice.lower()  
+        #This q/break will end the entire program
+        if userImageChoice == "q":
+            break     
+        #Shows the user the image they selected
+        elif userImageChoice in j_List:
+            userImage = Image.open(f"{userImageChoice}.jpg")
+            userImage.show()
+            x = userConfirm()
+            if x == False:
+                continue
+            elif x == True: 
+                alterUserImage()
+        #Any invalid user inputs will be caught by this else statement
+        else:
+            print("Invalid input\n")
+    
+    
+#This function asks the user for the alteration they would like to make to their selected image
 def alterUserImage():
     while True:
         print("Alter options: ")
+        #Displays the a(lteration) list just liek the j_List
         displayList(a_List)
         userChoice = input("How would you like to alter the image?: ")
         userChoice.lower()
+        #The user's choice will be compared to the a list to see which alteration is selected 
         if userChoice in a_List:
             if userConfirm() == True:
                 if userChoice == a_List[0]:
@@ -70,6 +93,7 @@ def alterUserImage():
                     blur()   
                     break
                 elif userChoice == a_List[5]:
+                    #If the user chooses to merge images, they will be asks to choose another image which is saved in a list
                     while True:
                         displayList(j_List)
                         userImageChoice2 = input("Which image would u like to merge with your orginal choice?: ")
@@ -79,27 +103,39 @@ def alterUserImage():
                             break
                         else:
                             print("Invalid input\n")  
+                    #The saved list is then used as a parameter to merge the images
                     merge(m_list)
                     break
                 elif userChoice == a_List[6]:
                     contrast()   
                     break
+        #If the user's input is not in the a list they will be told it is an invalid input and prompted again
         else:
             print("Invalid input\n")
 
+
+#This function will rotate the image to how many degrees the user inputs then save it in the corresponding folder
 def rotate():
     while True:
+        #This asks the user for how many degrees they want the picture to rotate
         try:
             userDegrees = int(input("How many degrees would you like to rotate?: "))
             break
         except ValueError:
             print("Invalid input\n")
     im = Image.open(f"{userImageChoice}.jpg")
+    #This rotates the picture the user inputed degrees
     im.rotate(userDegrees).save('rotation folder/'+userImageChoice+'Rotated.jpg')
     print("Saved in rotation folder\n")
 
+
+#This function will create a thumbnail to the size(200/400/600) the user chooses then save it in the corresponding folder
 def resize():
+    size200 = (200,200)
+    size400 = (400,400)
+    size600 = (600,600)
     while True:
+        #This asks the user for 1 of 3 sizes then saves a thumbnail of the chosen size
         userChoice = int(input("Do you want it to be 200, 400, 600: "))
         im = Image.open(f"{userImageChoice}.jpg")
         if userChoice == 200:
@@ -120,18 +156,24 @@ def resize():
         else:
             print("Invalid input")
 
+
+#This function will change the image file from a jpg to a png then save it in the corresponding folder
 def png():
     im = Image.open(f"{userImageChoice}.jpg")
     fn, fext = os.path.splitext(f"{userImageChoice}.jpg")
     im.save('png folder/{}.png'.format(fn))
     print("Saved in png folder\n")
 
+
+#This function will change the image to black and white then save it in the corresponding folder
 def blackWhite():
     im = Image.open(f"{userImageChoice}.jpg")
     im = im.convert("L")
     im.save('bw folder/'+userImageChoice+'BW.jpg')
     print("Saved in bw folder\n")
     
+  
+#This function will blur the image how much the user wants then save it in the corresponding folder   
 def blur():
     while True:
         try:
@@ -144,6 +186,8 @@ def blur():
     im1.save('blur folder/'+userImageChoice+'Blur.jpg')
     print("Saved in blur folder\n")
 
+
+#This will merge the first and second chosen images together then save it in the corresponding folder
 def merge(list):
     images = [Image.open(x) for x in list]
     widths, heights = zip(*(i.size for i in images))
@@ -156,11 +200,14 @@ def merge(list):
         x_offset += im.size[0]
     im1.save('merge folder/'+userImageChoice+'Merge.jpg')
     print("Saved in merge folder\n")
-    
+   
+  
+#This function will greatly increase the contrast of an image then save it in the corresponding folder   
 def contrast():
     im = Image.open(f"{userImageChoice}.jpg")
     enh = ImageEnhance.Contrast(im)
     enh.enhance(2.1).save('contrast folder/'+userImageChoice+'Contrast.jpg')
     print("Saved in contrast folder\n")
+
 
 main()
